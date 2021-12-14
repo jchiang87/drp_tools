c674ae7e846b1960e5a57472b9afe3aa4ea45b5d
Jim Chiang <jchiang@slac.stanford.edu>
2021-12-13 17:44:17 -0800

    refactor code to generate SFP bps yaml files

---
+++ python/desc/drp_tools/sfp_utils.py
@@ -0,0 +1,102 @@
+"""
+Code to generate bps yaml files for single frame processing.
+"""
+import os
+import sqlite3
+import numpy as np
+import pandas as pd
+
+__all__ = ['SfpYamlFactory']
+
+# Template for making bps yaml files.  Use '$(...)' for env vars to be
+# resolved by bps.  The '$()' will be replaced by '${}' after the
+# local python variables have been inserted.
+BPS_SFP_YAML = """includeConfigs:
+  - $(GEN3_WORKFLOW_DIR)/python/desc/gen3_workflow/etc/bps_drp_baseline.yaml
+  - $(GEN3_WORKFLOW_DIR)/examples/bps_DC2-3828-y1_resources.yaml
+
+pipelineYaml: "$(OBS_LSST_DIR)/pipelines/imsim/DRP.yaml#singleFrame"
+
+payload:
+  inCollection: LSSTCam-imSim/defaults
+  payloadName: {payloadName}
+  butlerConfig: {repo}
+  dataQuery: "{dataQuery}"
+"""
+
+class SfpYamlFactory:
+    """
+    Class to generate bps yaml files for single frame processing given
+    an sqlite3 db of the overlaps between ccd-visits and skymap tracts
+    and patches.
+    """
+    def __init__(self, overlap_db, repo):
+        """
+        Parameters
+        ----------
+        overlap_db : sqlite3 db file
+            This file contains an 'overlaps' table with the ccd-visits
+            that overlap each patch in the repo skymap.
+        """
+        if not os.path.isfile(overlap_db):
+            raise FileNotFoundError(f'{overlap_db} not found')
+        self.overlap_db = overlap_db
+        self.repo = repo
+
+    def create(self, tract, num_parts=1, visit_range=None,
+               processed_visits=None):
+        """
+        Create bps yaml files for single-frame processing of the
+        visits that overlap with the specified tract.
+
+        Parameters
+        ----------
+        tract : int
+            Tract number in the repo skymap.
+        num_parts : int [1]
+            Number of parts to divide processing among the corresponding
+            number of bps yaml files.
+        visit_range : (int, int) [None]
+            Range of visits to consider.
+        processed_visits : list [None]
+            List of visits that have already been processed and which
+            should be excluded.
+
+        Returns
+        -------
+        list of visits included in the bps yaml files.
+        """
+        repo = self.repo
+        if num_parts < 1:
+            raise ValueError('Must have num_parts >= 1.')
+        query = f'select * from overlaps where tract={tract}'
+        if visit_range is not None:
+            query += (f' and visit >= {visit_range[0]}'
+                      f' and visit <= {visit_range[1]}')
+        if processed_visits is not None and processed_visits:
+            processed_visit_list \
+                = '(' + ','.join([str(_) for _ in processed_visits]) + ')'
+            query += f' and visit not in {processed_visit_list}'
+        with sqlite3.connect(self.overlap_db) as con:
+            df0 = pd.read_sql(query, con)
+        visits = sorted(list(set(df0['visit'])))
+
+        indexes = np.linspace(0, len(visits) + 1, num_parts + 1, dtype=int)
+
+        for part, (imin, imax) in enumerate(zip(indexes[:-1], indexes[1:])):
+            # Use '[...]' here, and replace with '(...)' after env var
+            # delimiters have been replaced.
+            visit_list = '[' + ','.join([str(_) for _ in visits[imin:imax]]) \
+                         + ']'
+            payloadName = f'sfp_Y1_{tract}_visits_part_{part:02d}'
+            dataQuery = f"instrument='LSSTCam-imSim' and visit in {visit_list}"
+            outfile = f'bps_{payloadName}.yaml'
+            print(outfile)
+            with open(outfile, 'w') as output:
+                output_string \
+                    = BPS_SFP_YAML.format(**locals())\
+                                  .replace('(', '{').replace(')', '}')\
+                                  .replace('[', '(').replace(']', ')')
+                output.write(output_string)
+
+        return visits
